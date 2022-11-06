57018e9d35f0a78ee48bc7d0fc5e01063f5f4408
Karechta <43279761+Karechta@users.noreply.github.com>
2022-11-06 12:17:05 +0100

    move serializer.py's functions inside of client.py
    and get rid of serializer.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from client import Client
+from pwn import context
+context.log_level = 'debug'
 
 if __name__ == '__main__':
-    client = Client()
-    client.TryConnect("localhost")
+    client = Client("localhost", 8888, "Karechta")
+    client.Run()
     
