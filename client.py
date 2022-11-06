93c87e2315b6386bbcf07d857e992c463791abac
Karechta <43279761+Karechta@users.noreply.github.com>
2022-11-06 21:05:37 +0100

    Update client.py

--- client.py
+++ client.py
@@ -13,6 +13,11 @@ class Client:
     m_my_name   = None
     m_my_id     = None
 
+    m_grid_width = 0
+    m_grid_height = 0
+    m_playercount = 0
+    m_players = []
+
     def __init__(self, adress: str, port: int, name: str):
         self.m_server   = adress
         self.m_port     = port
@@ -42,22 +47,43 @@ class Client:
     #Handle Serverpackets
     def HandleReceive(self):
         try:
-            magic, = unpack("!I", self.m_socket.recvn(4))
+            magic = deserializeInt(self.m_socket, 32)
 
             match NetDataType(magic):
                 case NetDataType.CONNECTACK: #Server sends ID to us
-                    self.m_my_id = deserializeByte(self.m_socket)
+                    self.m_my_id = deserializeInt(self.m_socket,8)
                     self.m_connected = True
-                    print("Id erhalten")
                 
                 case NetDataType.BROADCAST: #Server sends a String to us, lets print it
                     print(deserializeString(self.m_socket))
                 
                 case NetDataType.GAMESTART:
-                    return "TODO"
+                    self.m_grid_width, self.m_grid_height = deserializeGrid(self.m_socket)
+                    self.m_playercount = deserializeInt(self.m_socket, 32)
+                    self.m_players.clear()
+                    for _ in range(self.m_playercount):
+                        player = {
+                            "id": deserializeInt(self.m_socket, 8),
+                            "name": deserializeString(self.m_socket)    
+                        }
+                        self.m_players.append(player)
                 
                 case NetDataType.GAMEDATA:
-                    return "TODO"
+                    playerturn = deserializeInt(self.m_socket, 8)
+                    epoch = deserializeInt(self.m_socket, 64)  
+                    fields = deserializeInt(self.m_socket, 32)
+                    for _ in range(fields):
+                        pos_x, pos_y = deserializeGrid(self.m_socket)
+                        type = deserializeInt(self.m_socket, 8)
+                        owner = deserializeInt(self.m_socket, 8)
+                        power = deserializeInt(self.m_socket, 16, False)
+                        #TODO Feld updaten 
+                    next_food = deserializeInt(self.m_socket, 32)
+                    for _ in range(next_food):
+                        pos_x, pos_y = deserializeGrid(self.m_socket)
+                        
+                           
+
         except:
             traceback.print_exc()
             self.m_connected = False
@@ -70,26 +96,41 @@ class Client:
                  return packet + serializeString(args[0])
             
             case NetDataType.LEAVE:
-                return "TODO"
+                return packet
             
             case NetDataType.MOVE:
-                return "TODO"
+                return serializeMove(args[0], args[1], args[2], args[3], args[4])
             
             case NetDataType.ENDTURN:
-                return "TODO"
+                return packet
             
             case NetDataType.BROADCAST:
-                return "TODO"    
+                return packet + serializeString(args[0])    
 
 
 def serializeString(s: str):
     b = s.encode("utf-8")
     return pack("!I", len(b)) + b
 
+def serializeMove(b:bool, from_x: int, from_y: int, to_x:int, to_y:int):
+    return pack("!bHHHH", b, from_x, from_y, to_x, to_y)
+
 def deserializeString(socket: remote):
     l, = unpack("!I", socket.recvn(4))
     return socket.recvn(l).decode("utf-8")
 
-def deserializeByte(socket: remote):
-    b, = unpack("!B", socket.recvn(1))
-    return b
+def deserializeGrid(socket: remote):
+    w,h = unpack("!HH", socket.recvn(4))
+    return w,h
+
+def deserializeInt(socket:remote, size: int, signed:bool = False):
+    f = unpack_formats[size]
+    if signed: f.lower()
+    i, = unpack ("!" + f, socket.recvn(size/8))
+
+unpack_formats = {
+    8: "B",
+    16: "H",
+    32: "I",
+    64: "Q"
+}
